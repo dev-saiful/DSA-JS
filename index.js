d32a86e57af41b4d6bbf5146d2685d4ed6c8bb3c
dev-saiful <saifulcseian@gmail.com>
2023-07-16 15:55:49 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-class LinkedList
-  {
-    constructor(value)
-    {
-      this.head = {
-        value : value,
-        next : null,
-      }
-      this.tail = this.head;
-      this.length = 1;
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
     }
-
-    append(value)
-    {
-      const newNode = {
-        value : value,
-        next : null,
-      }
-      this.tail.next = newNode;
-      this.tail = newNode;
-      this.length++;
-      return this;
-    }
-
-    prepend(value)
-    {
-      const newNode = {
-        value : value,
-        next : null,
-      }
-      newNode.next = this.head;
-      this.head = newNode;
-      this.length++;
-      return this;
-    }
+    this.tail = this.head;
+    this.length = 1;
   }
+
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  insert(index,value)
+  {
+    
+  }
+
+}
 
 let myList = new LinkedList(16);
 myList.append(5);
